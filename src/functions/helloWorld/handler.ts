6997fcc37b8f2fbb7ab8720db20421e8d1d11f91
Derlys <47836230+derlysfernando@users.noreply.github.com>
2024-01-05 13:40:37 -0300

    Update handler.ts

--- src/functions/helloWorld/handler.ts
+++ src/functions/helloWorld/handler.ts
@@ -1,7 +1,7 @@
-import { middyfy } from "@libs/lambda";
+import { middyfy } from "../libs/lambda";
 
 const helloWorld = async () => {
-  return {hello: "Hello World"}
+  return { hello: "Hello World" };
 };
 
 export const main = middyfy(helloWorld);
